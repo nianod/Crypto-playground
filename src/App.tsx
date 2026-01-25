d214bceb9b801ec6153e43da82b877ed48e10f71
nianod <arnoldkk422@gmail.com>
2026-01-25 20:38:35 +0300

    initialized crypto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { Lock, LockOpen } from "lucide-react";
+import CryptoJS from "crypto-js";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState("encryption");
+  const [inputText, setInputText] = useState<string>('')
+  const [outputText, SetOutputText] = useState<string>("")
+  const [secretKey, setSecretKey] = useState('british-special-forces')
+  const [copyText, setCopyText] = useState<boolean>(false)
 
   const tabs = [
     {
@@ -17,9 +22,38 @@ const App = () => {
     },
   ];
 
+  const encrypt = () => {
+    const encryption = CryptoJS.AES.encrypt(inputText, secretKey).toString()
+    SetOutputText(encryption)
+  }
+
+  const decrypt = () => {
+    try{
+    const bytes = CryptoJS.AES.decrypt(inputText, secretKey)
+    const decryption = bytes.toString(CryptoJS.enc.utf8)
+    if(!decryption) {
+      alert('Invalid text or key')
+      return
+    }
+    SetOutputText(decryption)
+    }catch(err:any) {
+      alert('decryption faiuled')
+    }
+  }
+
+  const copy = async() => {
+    try{
+    await navigator.clipboard.writeText(outputText)
+    setCopyText(true)
+    setTimeout(() => setCopyText(false), 3000)
+    }catch(err:any) {
+      console.log('failed to copy')
+    }
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center pt-10">
-      {/* Tabs */}
+ 
       <div className="flex gap-4 bg-gray-300 p-3 rounded-xl">
         {tabs.map((tab) => (
           <button
@@ -38,34 +72,37 @@ const App = () => {
         ))}
       </div>
 
-      {/* Content Area */}
-      <div className="mt-10 w-full max-w-xl p-6 bg-gray-100 rounded-xl">
+       <div className="mt-10 w-full max-w-xl p-6 bg-gray-100 rounded-xl">
         {activeTab === "encryption" && (
           <div className="flex flex-col md:flex-row gap-6 w-full">
-            {/* Input */}
-            <div className="flex flex-col w-full">
+             <div className="flex flex-col w-full">
               <textarea
+                onChange={(e) => setInputText(e.target.value) }
+                value={inputText}
                 className="w-full text-white h-40 p-3 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2   resize-none placeholder-gray-400"
                 placeholder="Enter your text here..."
               />
               <button
+                onClick={encrypt}
                 className="mt-3 py-2 rounded-md cursor-pointer bg-green-500  text-white font-semibold hover:bg-green-600 transition"
               >
                 Encrypt
               </button>
             </div>
 
-            {/* Output */}
-            <div className="flex flex-col w-full">
+             <div className="flex flex-col w-full">
               <textarea
+                 onChange={(e) => SetOutputText(e.target.value) }
+                 value={outputText}
                 className="text-gray-300 w-full h-40 p-3 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-600 rounded-md resize-none placeholder-gray-400"
                 placeholder="Encrypted text will appear here..."
                 readOnly
               />
               <button
+                onClick={copy}
                 className="mt-3 py-2 rounded-md bg-green-500 cursor-pointer text-white font-semibold hover:bg-green-600 transition"
               >
-                Copy
+                {copyText ? "Copied" : "Copy"}
               </button>
             </div>
           </div>
@@ -73,30 +110,34 @@ const App = () => {
 
         {activeTab === "decryption" && (
            <div className="flex flex-col md:flex-row gap-6 w-full">
-            {/* Input */}
-            <div className="flex flex-col w-full">
+             <div className="flex flex-col w-full">
               <textarea
+                onChange={(e) => setInputText(e.target.value) }
+                value={inputText}
                 className="w-full text-white h-40 p-3 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:ring-2   resize-none placeholder-gray-400"
                 placeholder="Enter your text here..."
               />
               <button
+                onClick={decrypt}
                 className="mt-3 py-2 rounded-md cursor-pointer bg-red-500  text-white font-semibold hover:bg-red-600 transition"
               >
                 Decrypt
               </button>
             </div>
 
-            {/* Output */}
-            <div className="flex flex-col w-full">
+             <div className="flex flex-col w-full">
               <textarea
+                onChange={(e) => SetOutputText(e.target.value) }
+                value={outputText}
                 className="text-gray-300 w-full h-40 p-3 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-600 rounded-md resize-none placeholder-gray-400"
                 placeholder="decrypted text will appear here..."
                 readOnly
               />
               <button
+                onClick={copy}
                 className="mt-3 py-2 rounded-md bg-red-500 cursor-pointer text-white font-semibold hover:bg-red-600 transition"
               >
-                Copy
+                {copyText ? "Copied" : "Copy"}
               </button>
             </div>
           </div>
